0d4bcba96321d4c3fdaa1d2f1e76259f4517059b
Agatha <agrabowska@wi.zut.edu.pl>
2012-11-26 21:07:25 +0100

    BookingSystem

---
+++ BookingSystem/application/controllers/IndexController.php
@@ -0,0 +1,59 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function homeAction()
+    {
+        // action body
+    }
+
+    public function ideaAction()
+    {
+        // action body
+    }
+
+    public function lecturesAction()
+    {
+        // action body
+    }
+
+    public function equipmentAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        // action body
+    }
+
+    public function contactAction()
+    {
+        // action body
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
