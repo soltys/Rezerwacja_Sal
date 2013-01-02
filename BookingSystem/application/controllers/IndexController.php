0c04e5cd26e7809f8ff97034c2b260e5fba01827
Patryk Diak <pdiak@wi.zut.edu.pl>
2013-01-02 16:55:21 +0100

    Podstawowa wersja fullcallendar

--- BookingSystem/application/controllers/IndexController.php
+++ BookingSystem/application/controllers/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
        $users = new Application_Model_DbTable_UserDb(); 
+
        /* metoda zwraca cała zawartość bazy danych */
        $this->view->users = $users->fetchAll();  
     }
@@ -27,7 +28,19 @@ class IndexController extends Zend_Controller_Action
 
     public function lecturesAction()
     {
-        // action body
+        $view = $this->view;
+        $view->addHelperPath("Vapps/View/Helper/",'Vapps_View_Helper');
+        $view->addHelperPath("ZendX/JQuery/View/Helper", "ZendX_JQuery_View_Helper");
+        $view->jQuery()->addStylesheet('js/jquery/css/smoothness/jquery-ui-1.9.2.custom.css');
+//                $view->jQuery()->addStylesheet('js/fullcalendar/fullcalendar.print.css');
+        $view->jQuery()->addStylesheet('js/fullcalendar/fullcalendar.css');
+        $view->jQuery()->setLocalPath('js/jquery/js/jquery-1.8.3.js');
+        $view->jQuery()->setUiLocalPath('js/jquery/js/jquery-ui-1.9.2.custom.js');
+        $view->jQuery()->addJavascriptFile('js/fullcalendar/fullcalendar.js');
+        $view->jQuery()->enable();
+        $view->jQuery()->uiEnable();
+        $events = new Application_Model_DbTable_EventsDb();
+        $this->view->eventsData = $events->getEventsJson();
     }
 
     public function equipmentAction()
---
+++ BookingSystem/application/models/DbTable/EventsDb.php
@@ -0,0 +1,47 @@
+<?php
+
+class Application_Model_DbTable_EventsDb extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'events';
+    public function getEvent($id) {
+        $id = (int) $id;
+        $row = $this->feetchRow('id = ' . $id);
+        if (!$row) {
+            throw new Exception("Could not find row $id");
+        }
+        return $row->toArray();
+    }
+    public function getEventsJson()
+    {
+        $select = $this->select();
+        $select->from($this,(array('title','start','end','allDay')));
+        return json_encode($this->fetchAll($select)->toArray(),JSON_NUMERIC_CHECK);
+        
+    }
+
+    public function addEvent($title, $start, $end) {
+        $data = array(
+            'title' => $title,
+            'start' => $start,
+            'end' => $end,
+        );
+        $this->insert($data);
+    }
+
+    public function updateEvent($title, $start, $end) {
+        $data = array(
+            'title' => $title,
+            'start' => $start,
+            'end' => $end,
+        );
+        $this->update($data, 'id = ' . (int) $id);
+    }
+
+    public function deleteEvent($id) {
+        $this->delete('id = ' . $id);
+        
+    }
+
+}
+
